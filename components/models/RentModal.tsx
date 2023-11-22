3945ece8e61f6dc901f5cf864bfa5361596638ba
Harshita <harshitabetala7@gmail.com>
2023-11-22 21:18:02 +0530

    List property updated

--- components/models/RentModal.tsx
+++ components/models/RentModal.tsx
@@ -212,8 +212,8 @@ function RentModal({ }: Props) {
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading
-          title="Now, set your price"
-          subtitle="How much do you charge per hour?"
+          title="Your Property Details"
+          subtitle="Set an inviting title and subtitle for your property"
         />
         <Input
           id="title"
